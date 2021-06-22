18234e7e933a1bdf81c036d6178fb3ec97fc1451
João Guilherme <joao.sczip@hotmail.com>
2021-06-22 10:41:10 -0300

    refactor: update main structure to use AWSParser

--- services/proxy/main.go
+++ services/proxy/main.go
@@ -1,31 +1,55 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"laws/lib"
+	"laws/lib/interfaces"
+	"laws/lib/parsers"
 	"net/http"
 	"os"
 	"regexp"
 )
 
-func ParseAuthHeader(authHeader string) (string, string) {
+func _ParseAuthHeader(authHeader string) (string, string) {
 	regexMatch := regexp.MustCompile(`20\d*\/(?P<region>\w*-\w*-\d)\/(?P<service>\w*)\/`)
 	match := regexMatch.FindStringSubmatch(authHeader)
 	return match[1], match[2]
 }
 
-func GetAuthHeader(res http.ResponseWriter, req *http.Request) {
+func _GetAWSParser(awsRequest lib.AwsRequest) (interfaces.AWSParser, error) {
+	parsersMap := map[string]interfaces.AWSParser{
+		"s3":  parsers.S3Parser{},
+		"sqs": parsers.SQSParser{},
+	}
+
+	if awsParser, keyExists := parsersMap[awsRequest.Service]; keyExists {
+		return awsParser, nil
+	}
+
+	return nil, errors.New("Service not found: " + awsRequest.Service)
+}
+
+func _GetAWSRequest(req *http.Request) lib.AwsRequest {
 	header := req.Header["Authorization"][0]
-	region, service := ParseAuthHeader(header)
-	awsrequest := lib.AwsRequest{
+	region, service := _ParseAuthHeader(header)
+	return lib.AwsRequest{
 		Region:  region,
 		Service: service,
 		Req:     req,
 	}
-	switch awsrequest.Service {
-	case "s3":
-		lib.S3Parse(awsrequest)
+}
+
+func _HandleRequest(res http.ResponseWriter, req *http.Request) {
+	awsRequest := _GetAWSRequest(req)
+	parser, err := _GetAWSParser(awsRequest)
+	if err != nil {
+		fmt.Println("Deu caca", err)
+		res.WriteHeader(http.StatusInternalServerError)
 	}
+
+	parser.Parse(awsRequest)
+	res.WriteHeader(http.StatusOK)
 }
 
 func main() {
@@ -36,6 +60,6 @@ func main() {
 	addr := fmt.Sprintf(":%s", port)
 	fmt.Printf("Server listening on port %s\n", port)
 
-	http.HandleFunc("/", GetAuthHeader)
+	http.HandleFunc("/", _HandleRequest)
 	http.ListenAndServe(addr, nil)
 }
